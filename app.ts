0ddd73973f8bef1e32a92cc7d46af1d9c6535915
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-06-12 19:42:58 +0100

    feat: add first configs in server routes

--- app.ts
+++ app.ts
@@ -1,4 +1,13 @@
-
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+
+app.set("view engine", "jade");
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/public"));
