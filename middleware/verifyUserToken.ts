d8edbea353cb66192fc548e88c0746f4421b53f1
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-06-14 09:26:29 +0100

    feat: add middleware to make requests

--- middleware/verifyUserToken.ts
+++ middleware/verifyUserToken.ts
@@ -5,27 +5,20 @@ import { randomUUID } from "crypto";
 import { isValidUser } from "../utils";
 import { RequestHandler } from "express";
 import { API } from "../types/userData.type";
-import { nextTick } from "process";
 
 const verifyUserToken: RequestHandler = async (req, res, next) => {
-  if (!req.headers.authorization) {
-    res.status(401).send("Unauthorized request");
-  }
-  const token = req.headers["authorization"]?.split(" ")[1];
+  const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    res.status(401).send("Acess denied. No token provided");
+    return res.status(401).send("Access denied. No token provided");
   }
 
   try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      next();
-    
+    jwt.verify(token, process.env.JWT_SECRET);
+    next();
   } catch (error) {
-      return res.status(400).send("Ivalid token!");
-    
+    return res.status(400).send("Ivalid token!");
   }
-
 };
 
 export default verifyUserToken;
